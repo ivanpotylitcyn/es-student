0790041d95ebc9b118c32922b8356549a9d1e612
Иван Потылицын <ivan.phystech@gmail.com>
2026-09-03 00:12:59 +0300

    Фикс трёх пробелов

--- 112-blink/main.c
+++ 112-blink/main.c
@@ -5,14 +5,14 @@ const uint LED_PIN = 25;
 
 int main()
 {
-   gpio_init(LED_PIN);
-   gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-   while (1)
-   {
-       gpio_put(LED_PIN, 1);
-       sleep_ms(250);
-       gpio_put(LED_PIN, 0);
-       sleep_ms(1000);
-   }
+    while (1)
+    {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(1000);
+    }
 }
