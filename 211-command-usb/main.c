083d4f576423841b0296faa6dac2d7804f212145
Иван Потылицын <ivan.phystech@gmail.com>
2026-09-19 13:03:58 +0300

    Сделана п2.1.2

--- 211-command-usb/main.c
+++ 211-command-usb/main.c
@@ -22,30 +22,67 @@ bool get_button_debounce(uint pin)
     return state && gpio_get(pin);
 }
 
+void cmd_enable(void)
+{
+    led_set(true);
+    LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+}
+
+void cmd_disable(void)
+{
+    led_set(false);
+    LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+}
+
+void cmd_info(void)
+{
+    device_info();
+}
+
+void cmd_version(void)
+{
+    log_version();
+}
+
+void cmd_ping(void)
+{
+    printf("pong\n");
+}
+
+typedef void (*command_handler_t)(void);
+
+struct command_t
+{
+    const char *name;
+    command_handler_t handler;
+};
+
+const struct command_t commands[] = {
+    {"enable", cmd_enable},
+    {"disable", cmd_disable},
+    {"info", cmd_info},
+    {"version", cmd_version},
+    {"ping", cmd_ping},
+};
+
+#define COMMAND_COUNT (sizeof(commands) / sizeof(commands[0]))
+
 void handle_command(const char *command)
 {
-    if (strcmp(command, "enable") == 0)
+    for (uint i = 0; i < COMMAND_COUNT; i++)
     {
-        led_set(true);
-        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+        if (strcmp(command, commands[i].name) == 0)
+        {
+            if (commands[i].handler != NULL)
+            {
+                commands[i].handler();
+            }
+
+            return;
+        }
     }
-    else if (strcmp(command, "disable") == 0)
-    {
-        led_set(false);
-        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
-    }
-    else if (strcmp(command, "info") == 0)
-    {
-        device_info();
-    }
-    else if (strcmp(command, "version") == 0)
-    {
-        log_version();
-    }
-    else
-    {
-        LOG_ERR("unknown command: %s\n", command);
-    }
+
+    LOG_ERR("unknown command: %s\n", command);
 }
 
 void read_line(void)
