90157edebe1324b073c25d411fdb0fb18416d82c
Иван Потылицын <ivan.phystech@gmail.com>
2026-09-21 00:57:01 +0300

    Сделана п2.1.4

--- 211-command-usb/main.c
+++ 211-command-usb/main.c
@@ -1,3 +1,5 @@
+#include "command.h"
+
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
@@ -55,13 +57,10 @@ void cmd_mem_info(void)
     mem_info();
 }
 
-typedef void (*command_handler_t)(void);
-
-struct command_t
+void cmd_fw_info(void)
 {
-    const char *name;
-    command_handler_t handler;
-};
+    fw_info();
+}
 
 const struct command_t commands[] = {
     { "enable", cmd_enable },
@@ -69,14 +68,15 @@ const struct command_t commands[] = {
     { "info", cmd_info },
     { "version", cmd_version },
     { "ping", cmd_ping },
-    { "mem_info", cmd_mem_info }
+    { "mem_info", cmd_mem_info },
+    { "fw_info", cmd_fw_info },
 };
 
-#define COMMAND_COUNT (sizeof(commands) / sizeof(commands[0]))
+const uint command_count = sizeof(commands) / sizeof(commands[0]);
 
 void handle_command(const char *command)
 {
-    for (uint i = 0; i < COMMAND_COUNT; i++)
+    for (uint i = 0; i < command_count; i++)
     {
         if (strcmp(command, commands[i].name) == 0)
         {
--- 211-command-usb/memory/memory.c
+++ 211-command-usb/memory/memory.c
@@ -1,10 +1,13 @@
 #include "memory.h"
+#include "command.h"
+#include "device.h"
 
 #include "pico/stdlib.h"
 #include "hardware/regs/addressmap.h"
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 extern char __flash_binary_start;
 extern char __flash_binary_end;
@@ -64,4 +67,49 @@ void mem_info(void)
     printf("  ram used    %8d = data %d + bss %d\n",
            ram_used_size, data_ram_size, bss_size);                                 // итог: занято в ОЗУ — .data и .bss
     printf("  ram free    %8d for heap and %d for stack\n", heap_size, stack_size); // итог: свободно в ОЗУ — под кучу и под стек
+}
+
+int main(void);
+
+uint32_t data_variable = 100;
+uint32_t bss_variable;
+
+void fw_info(void)
+{
+    data_variable++;
+    bss_variable++;
+
+    uint32_t stack_variable = 1946;
+    uint32_t *heap_variable = malloc(sizeof(uint32_t));
+
+    if (heap_variable != NULL)
+    {
+        *heap_variable = 1951;
+    }
+
+    printf("object          address     value\n");
+
+    uint16_t *main_code = (uint16_t *)((uintptr_t)main & ~1u);
+    uint16_t *fw_info_code = (uint16_t *)((uintptr_t)fw_info & ~1u);
+
+    printf("%-15s 0x%08x  0x%04x\n", "main", main, *main_code);
+    printf("%-15s 0x%08x  0x%04x\n", "fw_info", fw_info, *fw_info_code);
+
+    printf("%-15s 0x%08x\n", "commands", commands);
+
+    for (int i = 0; i < command_count; i++)
+    {
+        printf("- %-13s 0x%08x\n", commands[i].name, commands[i].handler);
+    }
+
+    printf("%-15s 0x%08x  %s\n", "DEVICE_PROJECT", &DEVICE_PROJECT, DEVICE_PROJECT);
+    printf("%-15s 0x%08x  %s\n", "DEVICE_BOARD", &DEVICE_BOARD, DEVICE_BOARD);
+
+    printf("%-15s 0x%08x  %d\n", "data_variable", &data_variable, data_variable);
+    printf("%-15s 0x%08x  %d\n", "bss_variable", &bss_variable, bss_variable);
+
+    printf("%-15s 0x%08x  %d\n", "stack_variable", &stack_variable, stack_variable);
+    printf("%-15s 0x%08x  %d\n", "heap_variable", heap_variable, *heap_variable);
+
+    free(heap_variable);
 }
